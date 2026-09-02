f71c892323ae81f24b08f072f413e7af8a823fa1
théo beland <belandtheo31@gmail.com>
2026-09-02 15:47:24 +0200

    main implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,18 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadSymptomDataFromFile read = new ReadSymptomDataFromFile("symptoms.txt");
+        WriteSymptomDataToFile write = new WriteSymptomDataToFile("result.out");
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(read, write);
+
+        List<String> symptoms = analyticsCounter.getSymptoms();
+        Map<String, Integer> countSymptoms = analyticsCounter.countSymptoms(symptoms);
+        Map<String, Integer> sortymptoms = analyticsCounter.sortSymptoms(countSymptoms);
+        analyticsCounter.writeSymptoms(sortymptoms);
+
+    }
+}
